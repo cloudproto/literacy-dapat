d72daf1a038b53ba1182a19e001c763f84fe1c5a
cloudproto <cloud.protogen@gmail.com>
2025-03-17 22:22:09 +0800

    www

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://literacy-dapat-backend-1drqcmykm-cloudprotos-projects.vercel.app";
+const API_URL = "https://literacy-dapat-backend-69sgvhi62-cloudprotos-projects.vercel.app";
 
 export const loginUser = async (email, password) => {
     try {
